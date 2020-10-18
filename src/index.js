d89f7cf7333f94970e2c3fcc6f8e000fe75297f2
RRoLL545 <motom@yandex.ru>
2020-10-18 21:47:49 +0400

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -37,8 +37,41 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
+function decode(str) {
     // write your solution here
+    let decoded = [];
+
+    let splitToWords = str.split('**********');
+    
+    for (let i = 0; i < splitToWords.length; i++ ) {
+     let word = (splitToWords[i]).match(/.{1,10}/g);
+     for ( let j = 0; j < word.length; j++ ) {
+     let letter = word[j].match(/.{1,2}/g);
+       for ( let k = 0; k < letter.length; k++ ) {
+         switch (letter[k]) {
+           case '00':
+           letter[k] = '';
+           break;
+           case '10':
+           letter[k] = '.';
+           break;
+           case '11':
+           letter[k] = '-';
+           break;
+         }
+       }
+       letter = letter.join('');
+       letter = MORSE_TABLE[letter];
+       word[j] = letter;
+     }
+     word = word.join('');
+     splitToWords[i] = word;
+    }
+   
+   decoded = splitToWords.join(' ');
+    
+    
+    return decoded;
 }
 
 module.exports = {
